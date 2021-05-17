1d80bbc4b8dafa2ee3fa7b58d53c2430c62a04f3
Hernando <ginkotsu17@gmail.com>
2021-05-17 19:02:43 +0700

    controller Home, Dashboard, bisa add new account,

--- app/Views/dashboard/create.php
+++ app/Views/dashboard/create.php
@@ -14,10 +14,10 @@
                     <h1 class="auth-title">Add New Account</h1>
                     <p class="auth-subtitle mb-5">Input your data to register to our website.</p>
 
-                    <form action="index.html">
+                    <form action="/Home/save" method="post" enctype="multipart/form-data">
                         <?= csrf_field(); ?>
 
-                        <select name="role" id="">
+                        <select name="level" id="">
                             <option value="master">Master</option>
                             <option value="admin">Admin</option>
                         </select></i>
@@ -25,34 +25,31 @@
                         <br>
                         <br>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Username">
+                            <input type="text" class="form-control form-control-xl <?= ($validation->hasError('username')) ? 'is-invalid' : ''; ?>" placeholder="Username" name="username" autofocus value="<?= old('username'); ?>">
                             <div class="form-control-icon">
                                 <i class="bi bi-envelope"></i>
                             </div>
                         </div>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Email">
+                            <input type="text" class="form-control form-control-xl <?= ($validation->hasError('email')) ? 'is-invalid' : ''; ?>" placeholder="Email" name="email" value="<?= old('email'); ?>">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
                         </div>
+
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <input type="password" class="form-control form-control-xl <?= ($validation->hasError('password')) ? 'is-invalid' : ''; ?>" placeholder="Password" name="password" value="<?= old('password'); ?>">
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
                         </div>
 
 
-                        <i><input class="form-control <?= ($validation->hasError('foto')) ? 'is-invalid' : ''; ?>" type="file" id="foto" name="foto"></i>
+                        <i><input class="form-control <?= ($validation->hasError('foto')) ? 'is-invalid' : ''; ?>" type="file" id="foto" name="foto" value="<?= old('foto'); ?>"></i>
 
 
-                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Sign Up</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Create</button>
                     </form>
-                    <div class="text-center mt-5 text-lg fs-4">
-                        <p class='text-gray-600'>Already have an account? <a href="auth-login.html" class="font-bold">Log
-                                in</a>.</p>
-                    </div>
                 </div>
             </div>
             <div class="col-lg-7 d-none d-lg-block">
--- app/Views/dashboard/pengguna.php
+++ app/Views/dashboard/pengguna.php
@@ -9,6 +9,8 @@
                 <p class="text-subtitle text-muted">For user to check they list</p>
             </div>
 
+
+
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
@@ -31,12 +33,13 @@
                                     <?= session()->getFlashdata('pesan'); ?>
                                 </div>
                             <?php endif; ?>
-                            <a href="/Home/create" class="mb-4 btn btn-success" style="float:right; margin-right:20px;margin-top:20px;">+</a>
+                            <a href="/Home/create" class="mb-4 btn btn-success fas fa-user-plus" style="float:right; margin-right:20px;margin-top:20px;"></a>
                             <table class="table table-bordered mb-0">
                                 <thead>
                                     <tr>
                                         <th>ID</th>
                                         <th>FOTO</th>
+                                        <th>LEVEL</th>
                                         <th>NAMA</th>
                                         <th>EMAIL</th>
                                         <th>ACTION</th>
@@ -49,7 +52,10 @@
                                                 <?= $k['id']; ?>
                                             </td>
                                             <td>
-                                                <?= $k['foto']; ?>
+                                                <img class="rounded-circle" style="margin-left:10px;" src="/assets/images/pp/<?= $k['foto']; ?>" width="50px">
+                                            </td>
+                                            <td>
+                                                <?= $k['level']; ?>
                                             </td>
                                             <td>
                                                 <?= $k['username']; ?>
@@ -59,20 +65,26 @@
                                             </td>
                                             <td>
                                                 <div class="buttons">
-                                                    <a href="#" class="btn btn-success">Edit</a>
-                                                    <a href="#" class="btn btn-warning">Reset</a>
-                                                    <a href="#" class="btn btn-danger">Delete</a>
-                                                </div>
+                                                    <a href="#" class="btn btn-primary fa fa-user"></a>
+                                                    <a href="#" class="btn btn-success fa fa-edit"></a>
+                                                    <a href="#" class="btn btn-warning fas fa-key"></a>
+                                                    <form action="/Home/<?= $k['id']; ?>" method="post" class="d-inline">
+                                                        <?= csrf_field(); ?>
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <button type="submit" class="btn btn-danger  fas fa-trash" onclick="return confirm('Apakah anda yakin?');"></button>
+                                                    </form>
                                             </td>
-                                        </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
                         </div>
+                        </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                    </table>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+</div>
+</section>
 
-    <?= $this->endSection(); ?>
+<?= $this->endSection(); ?>
