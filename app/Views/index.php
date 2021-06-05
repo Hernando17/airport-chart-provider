c5e249f09dd039c0b11cb6f3078808d78b764dea
Hernando <ginkotsu17@gmail.com>
2021-06-05 17:07:45 +0700

    ganti gambar

--- app/Views/index.php
+++ app/Views/index.php
@@ -18,28 +18,18 @@
     <main>
 
         <!-- slider Area Start-->
-        <div class="slider-area  slider-height" data-background="assets/img/hero/out.png">
+        <div class="slider-area  slider-height" data-background="assets/img/hero/airbus.png">
             <div class="slider-active">
                 <!-- Single Slider -->
                 <div class="single-slider">
                     <div class="slider-cap-wrapper">
                         <div class="hero__img">
-                            <img src="assets/img/hero/cockpit.png" alt="" data-animation="fadeInRight" data-transition-duration="5s">
+                            <img src="assets/img/hero/garudatakeoff.png" alt="" data-animation="fadeInRight" data-transition-duration="5s">
                         </div>
                         <div class="hero__caption">
                             <h1 data-animation="fadeInLeft" data-delay=".4s">Pilot Gaming Indo <br>Center</h1>
-                            <p style="font-color:red;" data-animation="fadeInLeft" data-delay=".6s">Memfasilitasi kebutuhan bagi para flightsimer serta memudahkan dalam pencarian informasi terkait dunia fligth simulator dari addon hingga livery </p>
+                            <p data-animation="fadeInLeft" data-delay=".6s">Memfasilitasi kebutuhan bagi para flightsimer serta memudahkan dalam pencarian informasi terkait dunia fligth simulator dari addon hingga livery </p>
                             <!-- Hero Btn -->
-                            <a href="#" class="hero-btn" data-animation="fadeInLeft" data-delay=".9s">
-                                View project
-                                <!-- Arrow SVG icon -->
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                                    <path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068
-                                         c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557
-                                         l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104
-                                         c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z" />
-                                </svg>
-                            </a>
                         </div>
                     </div>
                 </div>
